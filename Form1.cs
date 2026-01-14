a11c4582f1df60eade84b1dd39208b632ac1853c
razvan-rusan <rusanrazvan@gmail.com>
2026-01-14 23:45:44 +0200

    upd3

--- Form1.cs
+++ Form1.cs
@@ -11,16 +11,20 @@ namespace ModelGraficaFull
             mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             mainGraphics = Graphics.FromImage(mainBitmap);
 
-            PointFTransformation scale = (point) => new PointF(point.X * 1.3f - 100f, point.Y - 100f);
+            PointFTransformation birdTransform = (point) => new(point.X * 1.3f - 100f, point.Y - 100f),
+                                 smallTranslation = (p) => new(p.X+50, p.Y+23);
+            PointFTransformation currT = new(birdTransform);
+            for (int i = 0; i < 4; i++, currT = new(Transforms.Compose(currT, smallTranslation))) {
+                Bezier.DrawBezierBird(
+                    new(Color.Black, 4),
+                    mainGraphics,
+                    new(202f, 248f),
+                    new(80f + i, 30f),
+                    currT,
+                    20
+                 );  
+            }
 
-            Bezier.DrawBezierBird(
-                new(Color.Black, 4), 
-                mainGraphics, 
-                new(202f,248f), 
-                new(80f, 30f), 
-                scale,
-                20);
-            
             pictureBox1.Image = mainBitmap;
         }
 
---
+++ Transforms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelGraficaFull
+{
+    public static class Transforms
+    {
+        public static PointFTransformation Rotate(float radius, float angle) {
+            return (PointF p) => new(p.X + radius * MathF.Sin(angle), p.Y + radius * MathF.Cos(angle));
+        } 
+
+        public static PointFTransformation Translate(float dx, float dy) {
+            return (PointF p) => new(p.X + dx, p.Y + dy);
+        }
+
+        public static PointFTransformation Scale(float scl)
+        {
+            return (PointF p) => new(p.X * scl, p.Y * scl);
+        }
+
+        public static PointFTransformation Scale(float sclX, float sclY)
+        {
+            return (PointF p) => new(p.X * sclX, p.Y * sclY);
+        }
+
+        public static PointFTransformation Compose(PointFTransformation a, PointFTransformation b)
+        {
+            return (PointF p) => a(b(p));
+        }
+    }
+}
