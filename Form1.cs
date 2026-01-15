c4b28bb477b146ca6078c255cdbfa20ae9f444fe
razvan-rusan <rusanrazvan@gmail.com>
2026-01-15 02:09:42 +0200

    upd5

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using static ModelGraficaFull.LSystems;
 
 namespace ModelGraficaFull
@@ -6,16 +7,73 @@ namespace ModelGraficaFull
     {
         Graphics mainGraphics;
         Bitmap mainBitmap;
-
+        readonly Random random = new();
+        readonly OklchColor SkyBlue = new(0.632f, 0.1352f, 215f);
+        readonly OklchColor SunsetFuchsia = new(0.7053f, 0.1574f, 32.4f);
+        readonly OklchColor GrassGreen = new(0.5635f, 0.145f, 135.11f);
+        readonly Brush shadow = new SolidBrush(Color.FromArgb(80, Color.Black));
         public CasaMeaDeLaTara()
         {
             InitializeComponent();
             mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             mainGraphics = Graphics.FromImage(mainBitmap);
 
+            #region Gradient
+            float
+                endFuchsia = pictureBox1.Height * 0.40f,
+                endBlue = pictureBox1.Height * 0.80f,
+                fuchsiaSep = 0.4f,
+                blueSep = 0.6f;
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    OklchColor fillC;
+                    if (y >= 0 && y <= endFuchsia * fuchsiaSep)
+                    {
+                        fillC = SunsetFuchsia;
+                    }
+                    else if (y > endFuchsia * fuchsiaSep && y <= endFuchsia)
+                    {
+                        fillC = ColorUtility.Mix(y, endFuchsia * fuchsiaSep + 1, endFuchsia, SunsetFuchsia, SkyBlue);
+
+                    }
+                    else if (y > endFuchsia && y <= endBlue * blueSep)
+                    {
+                        fillC = SkyBlue;
+                    }
+                    else if (y > blueSep)
+                    {
+                        fillC = ColorUtility.Mix(y, endBlue * blueSep + 1, endBlue, SkyBlue, GrassGreen);
+                    }
+                    else
+                    {
+                        fillC = GrassGreen;
+                    }
+                    mainBitmap.SetPixel(x, y, fillC);
+                }
+            }
+            #endregion
             #region Sun
             Sun s = new Sun(mainGraphics, pictureBox1.Width/2-310, pictureBox1.Height/2-230, 80f, 30);
             s.Draw();
+            #endregion
+            #region Clouds
+            const int totalClouds = 23;
+            for (int i = 0; i < totalClouds; i++)
+            { 
+                float xOffset = (float)random.NextDouble() * 60;
+                float yOffset = (float)random.NextDouble() * 40;
+                
+                if (random.NextSingle() < 0.67f) //randomly make clouds missing
+                {
+                    continue;
+                }
+
+                XYTransformation cloudTransform = (x, y) => (x + xOffset, y + yOffset);
+                Gosper gosper = new Gosper(mainGraphics, "A", MathF.PI / 3, new PointF(i*(pictureBox1.Width/ totalClouds), pictureBox1.Height / 2 - 50f - (float)random.NextDouble() * 20f), 20f, 4);
+                gosper.Draw(new Pen(Color.FromArgb(20, Color.White), 10), cloudTransform);
+            }
             #endregion
             #region Birds
             PointFTransformation birdTransform = (point) => new(point.X * 1.3f - 100f, point.Y - 100f),
@@ -32,14 +90,84 @@ namespace ModelGraficaFull
                  );  
             }
             #endregion
-            #region Trees
-            //Gosper gosper = new Gosper(mainGraphics, "A", MathF.PI / 3, new PointF(pictureBox1.Width / 2, pictureBox1.Height / 2), 8f, 4);
-            //gosper.Draw();
-            AcaciaTree tree = new AcaciaTree(mainGraphics, "X", MathF.PI/8, new(300f, 300f), 40f, 3);
-            tree.Draw();
-            Algae1 algae1 = new Algae1(mainGraphics, "A", MathF.PI / 7, new(100f, 400f), 10f, 5);
-            algae1.Draw();
+            #region House
+            using (Bitmap woodImage = new Bitmap("C:\\Users\\razvan\\source\\repos\\ModelGraficaFull\\minecraft_planks.png"))
+            {
+                using (TextureBrush woodBrush = new TextureBrush(woodImage)) { 
+                    woodBrush.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
+                    // front rectangle dimensions
+                    float x0 = pictureBox1.Width * 0.05f;
+                    float y0 = pictureBox1.Height * 0.6f;
+                    float w = 250f;
+                    float h = 200f;
+                    // depth for the right-side face (isometric offset)
+                    float depth = 60f;
+
+                    // side
+                    mainGraphics.FillPolygon(woodBrush, new PointF[]
+                    {
+                        new PointF(x0, y0),                          // top-left front
+                        new PointF(x0 + w, y0),                      // top-right front
+                        new PointF(x0 + w + depth, y0 - depth * 0.4f), // top-right back (slanted)
+                        new PointF(x0 + w + depth, y0 + h - depth * 0.4f), // bottom-right back
+                        new PointF(x0 + w, y0 + h),                  // bottom-right front
+                        new PointF(x0, y0 + h)                       // bottom-left front
+                    });
+                    mainGraphics.FillPolygon(shadow, new PointF[]
+                    {
+                        new PointF(x0, y0),                          // top-left front
+                        new PointF(x0 + w, y0),                      // top-right front
+                        new PointF(x0 + w + depth, y0 - depth * 0.4f), // top-right back (slanted)
+                        new PointF(x0 + w + depth, y0 + h - depth * 0.4f), // bottom-right back
+                        new PointF(x0 + w, y0 + h),                  // bottom-right front
+                        new PointF(x0, y0 + h)                       // bottom-left front
+                    });
+                    // front face
+                    mainGraphics.FillRectangle(woodBrush, x0, y0, w + 20f, h);
+                }
+            }
+            using (Bitmap brickImage = new Bitmap("C:\\Users\\razvan\\source\\repos\\ModelGraficaFull\\minecraft_bricks.png"))
+            {
+                using (TextureBrush brickBrush = new TextureBrush(brickImage))
+                {
+                    brickBrush.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
+                    brickBrush.ScaleTransform(0.3f, 0.3f);
+                    // roof geometry matching the house front for a simple 3D impression
+                    float x0 = pictureBox1.Width * 0.05f;
+                    float y0 = pictureBox1.Height * 0.6f;
+                    float w = 250f;
+                    // use the same peak Y as before
+                    float roofPeakY = pictureBox1.Height * 0.45f;
+                    float roofRise = y0 - roofPeakY;
+                    float depth = 60f;
+                    float xOff = 20f, xOff2 = 0f;
+
+                    mainGraphics.FillPolygon(brickBrush, new PointF[]
+                    {
+                        new PointF(x0 - 20f - xOff2, y0),
+                        new PointF(x0 + w + 20f, y0),
+                        new PointF(x0 + w + depth, y0 - depth * 0.4f),
+                        new PointF(x0 + w * 0.5f + depth - xOff, y0 - roofRise - depth * 0.4f),
+                    });
+                    mainGraphics.FillPolygon(shadow, new PointF[]
+                    {
+                        new PointF(x0 - 20f - xOff2, y0),
+                        new PointF(x0 + w + 20f, y0),
+                        new PointF(x0 + w + depth, y0 - depth * 0.4f),
+                        new PointF(x0 + w * 0.5f + depth - xOff, y0 - roofRise - depth * 0.4f),
+                    });
+                    mainGraphics.FillPolygon(brickBrush, new PointF[]
+                    {
+                        new PointF(x0 - 20f - xOff2, y0),
+                        new PointF(x0 + w + 20f, y0),
+                        //new PointF(x0 + w + depth, y0 - depth * 0.4f),
+                        new PointF(x0 + w * 0.5f + depth - xOff , y0 - roofRise - depth * 0.4f),
+                    });
+                }
+            }
+            
             #endregion
+            
             pictureBox1.Image = mainBitmap;
         }
 
--- LSystems.cs
+++ LSystems.cs
@@ -84,6 +84,81 @@ namespace ModelGraficaFull
                 }
             }
 
+            public void Draw(Pen p)
+            {
+                PointF disp = new PointF(0, this.segmentLength);
+                for (int i = 0; i < this.contents.Length; i++)
+                {
+                    float iX = pos.X;
+                    float iY = pos.Y;
+                    switch (this.contents[i])
+                    {
+                        case 'A':
+                        case 'B':
+                            {
+                                pos.X += disp.X;
+                                pos.Y += disp.Y;
+                                break;
+                            }
+                        case '+':
+                            {
+                                currentAngle += angle;
+                                disp.X = MathF.Cos(currentAngle) * this.segmentLength;
+                                disp.Y = MathF.Sin(currentAngle) * this.segmentLength;
+                                break;
+                            }
+                        case '-':
+                            {
+                                currentAngle -= angle;
+                                disp.X = MathF.Cos(currentAngle) * this.segmentLength;
+                                disp.Y = MathF.Sin(currentAngle) * this.segmentLength;
+                                break;
+                            }
+                    }
+                    g.DrawLine(p, iX, iY, pos.X, pos.Y);
+                }
+            }
+
+            public void Draw(Pen p, XYTransformation tr)
+            {
+                PointF disp = new PointF(0, this.segmentLength);
+                for (int i = 0; i < this.contents.Length; i++)
+                {
+                    float iX = pos.X;
+                    float iY = pos.Y;
+                    switch (this.contents[i])
+                    {
+                        case 'A':
+                        case 'B':
+                            {
+                                pos.X += disp.X;
+                                pos.Y += disp.Y;
+                                break;
+                            }
+                        case '+':
+                            {
+                                currentAngle += angle;
+                                disp.X = MathF.Cos(currentAngle) * this.segmentLength;
+                                disp.Y = MathF.Sin(currentAngle) * this.segmentLength;
+                                break;
+                            }
+                        case '-':
+                            {
+                                currentAngle -= angle;
+                                disp.X = MathF.Cos(currentAngle) * this.segmentLength;
+                                disp.Y = MathF.Sin(currentAngle) * this.segmentLength;
+                                break;
+                            }
+                    }
+                    float jX, jY;
+                    (iX, iY) = tr(iX,iY);
+                    (jX, jY) = tr(pos.X, pos.Y);
+                    g.DrawLine(p, iX, iY, pos.X, pos.Y);
+                }
+            }
+
+            
+
             protected override void Update()
             {
                 String next = "";
@@ -106,198 +181,5 @@ namespace ModelGraficaFull
             }
         }
 
-        public class AcaciaTree(Graphics g, string axiom, float angle, PointF pos, float segmentLength, int numIterations)
-        : LSystem(g, axiom, angle, pos, segmentLength, numIterations)
-        {
-            private Stack<(PointF pos, float angle)> stack = new();
-
-            public override void Draw()
-            {
-                PointF disp = new PointF(MathF.Cos(currentAngle) * segmentLength, MathF.Sin(currentAngle) * segmentLength);
-                for (int i = 0; i < contents.Length; i++)
-                {
-                    switch (contents[i])
-                    {
-                        case 'F':
-                            {
-                                float nextX = pos.X + disp.X;
-                                float nextY = pos.Y + disp.Y;
-                                g.DrawLine(Pens.Black, pos.X, pos.Y, nextX, nextY);
-                                pos = new PointF(nextX, nextY);
-                                break;
-                            }
-                        case '+':
-                            {
-                                currentAngle += angle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                        case '-':
-                            {
-                                currentAngle -= angle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                        case '[':
-                            {
-                                stack.Push((pos, currentAngle));
-                                break;
-                            }
-                        case ']':
-                            {
-                                var (savedPos, savedAngle) = stack.Pop();
-                                pos = savedPos;
-                                currentAngle = savedAngle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                    }
-                }
-            }
-
-            protected override void Update()
-            {
-                String next = "";
-                for (int i = 0; i < contents.Length; i++)
-                {
-                    switch (contents[i])
-                    {
-                        case 'F':
-                            next += "FF";
-                            break;
-                        case 'X':
-                            next += "F+[[X]-X]-F[-FX]+X";
-                            break;
-                        default:
-                            next += contents[i];
-                            break;
-                    }
-                }
-                contents = next;
-            }
-        }
-
-        public class Algae1(Graphics g, string axiom, float angle, PointF pos, float segmentLength, int numIterations)
-        : LSystem(g, axiom, angle, pos, segmentLength, numIterations)
-        {
-            public override void Draw()
-            {
-                PointF disp = new PointF(segmentLength, 0);
-                for (int i = 0; i < contents.Length; i++)
-                {
-                    float iX = pos.X;
-                    float iY = pos.Y;
-                    switch (contents[i])
-                    {
-                        case 'A':
-                        case 'B':
-                            {
-                                pos.X += disp.X;
-                                pos.Y += disp.Y;
-                                break;
-                            }
-                        case '+':
-                            {
-                                currentAngle += angle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                        case '-':
-                            {
-                                currentAngle -= angle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                    }
-                    g.DrawLine(Pens.Green, iX, iY, pos.X, pos.Y);
-                }
-            }
-
-            protected override void Update()
-            {
-                String next = "";
-                for (int i = 0; i < contents.Length; i++)
-                {
-                    switch (contents[i])
-                    {
-                        case 'A':
-                            next += "AB";
-                            break;
-                        case 'B':
-                            next += "A";
-                            break;
-                        default:
-                            next += contents[i];
-                            break;
-                    }
-                }
-                contents = next;
-            }
-        }
-
-        public class Algae2(Graphics g, string axiom, float angle, PointF pos, float segmentLength, int numIterations)
-        : LSystem(g, axiom, angle, pos, segmentLength, numIterations)
-        {
-            public override void Draw()
-            {
-                PointF disp = new PointF(segmentLength, 0);
-                for (int i = 0; i < contents.Length; i++)
-                {
-                    float iX = pos.X;
-                    float iY = pos.Y;
-                    switch (contents[i])
-                    {
-                        case 'X':
-                        case 'Y':
-                            {
-                                pos.X += disp.X;
-                                pos.Y += disp.Y;
-                                break;
-                            }
-                        case '+':
-                            {
-                                currentAngle += angle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                        case '-':
-                            {
-                                currentAngle -= angle;
-                                disp.X = MathF.Cos(currentAngle) * segmentLength;
-                                disp.Y = MathF.Sin(currentAngle) * segmentLength;
-                                break;
-                            }
-                    }
-                    g.DrawLine(Pens.DarkGreen, iX, iY, pos.X, pos.Y);
-                }
-            }
-
-            protected override void Update()
-            {
-                String next = "";
-                for (int i = 0; i < contents.Length; i++)
-                {
-                    switch (contents[i])
-                    {
-                        case 'X':
-                            next += "X+YF++YF-FX--FXFX-YF+";
-                            break;
-                        case 'Y':
-                            next += "-FX+YFYF++YF+FX--FX-Y";
-                            break;
-                        default:
-                            next += contents[i];
-                            break;
-                    }
-                }
-                contents = next;
-            }
-        }
     }
 }
